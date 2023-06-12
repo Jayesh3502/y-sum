50edb8f452506b60bc689b331e1077e9fd1d7cfb
Jayesh Mehta <80670368+Jayesh3502@users.noreply.github.com>
2023-06-12 05:30:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ def index():
             youtube_video = request.form['youtube_video']
             video_id = youtube_video.split("=")[1]
             transcript = YouTubeTranscriptApi.get_transcript(video_id)
-            
+
             result = ""
             for i in transcript:
                 result += ' ' + i['text']
-            
+
             summarizer = pipeline('summarization')
-            
+
             num_iters = int(len(result) / 1000)
             summarized_text = []
             for i in range(num_iters + 1):
@@ -28,15 +28,15 @@ def index():
                 out = out[0]
                 out = out['summary_text']
                 summarized_text.append(out)
-            
+
             summarized_text = ' '.join(summarized_text)
-            
+
             return render_template('result.html', summarized_text=summarized_text)
-        
+
         except Exception as e:
             error_message = str(e)
             return render_template('error.html', error_message=error_message)
-    
+
     return render_template('index.html')
 
 if __name__ == '__main__':
